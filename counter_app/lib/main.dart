67b6f8c6f7a6e22067c7e1921344470e4de24b73
kalelfleith <kalelfleith03@gmail.com>
2022-04-07 23:14:35 -0300

    counter_app created

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,21 +1,42 @@
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors, camel_case_types
+
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    theme: ThemeData(primarySwatch: Colors.purple),
-    home: Scaffold(
+      theme: ThemeData(primarySwatch: Colors.purple), home: HomePage()));
+}
+
+class HomePage extends StatefulWidget {
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<HomePage> {
+  var count = 0;
+
+  void increment() {
+    count++;
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
       appBar: AppBar(
         title: Text("My App - Counter"),
       ),
       body: Center(
           child: Text(
-        "CONTADOR\n0",
+        "CONTADOR\n$count",
         textAlign: TextAlign.center,
       )),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: () {},
+        onPressed: () {
+          increment();
+        },
       ),
-    ),
-  ));
+    );
+  }
 }
